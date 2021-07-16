e6c2304204752784b1e1b4105343e0ae62d34060
Gagan-700 <gagan.jaiswal2@gmail.com>
2021-07-16 13:05:21 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,36 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
   Data: Array<any>;
+  approveFlag: boolean = false;
+  rejectFlag: boolean = false;
 
-  constructor(){
+  constructor() {
     this.Data = [
-      {Company:"Google",Contact:"Raju",Country:'US'},
-      {Company:"Google",Contact:"Raju",Country:'US'},
-      {Company:"Google",Contact:"Raju",Country:'US'},
-      {Company:"Google",Contact:"Raju",Country:'US'}
-    ]
+      { Company: 'Google', Contact: 'Raju', Country: 'US' },
+      { Company: 'Google', Contact: 'Raju', Country: 'US' },
+      { Company: 'Google', Contact: 'Raju', Country: 'US' },
+      { Company: 'Google', Contact: 'Raju', Country: 'US' }
+    ];
+  }
+
+  approve() {
+    console.log('heyyyy');
+    if (this.approveFlag === true) {
+      this.approveFlag = false;
+    } else {
+      this.approveFlag = true;
+    }
+  }
+
+  reject() {
+    if (this.rejectFlag === true) {
+      this.rejectFlag = false;
+    } else {
+      this.rejectFlag = true;
+    }
   }
 }
